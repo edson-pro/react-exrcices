d64cf56955a829d48cf7640718aa222346c34635
Ntwali Edson <ntwaliedson9@gmail.com>
2023-02-22 14:30:15 +0200

    fix: implemented comments

--- src/exercises/HelloWorld.jsx
+++ src/exercises/HelloWorld.jsx
@@ -1,9 +1,9 @@
-import React from "react";
-
-export default function HelloWorld() {
+const HelloWorld = () => {
   return (
     <div className="w-[500px] bg-yellow-500 flex items-center justify-center h-[500px]">
       HelloWorld
     </div>
   );
-}
+};
+
+export default HelloWorld
